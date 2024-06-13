83d79b7c12464d241e9764ead5a5ad8a8edb6a6f
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-06-13 22:14:41 +0800

    Cannot get healthcheck to work on OSRM

--- ui/app.py
+++ ui/app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('Route Generator')
 
-distance = st.slider("Distance (in km):", min_value=0, max_value=100, value=10, step=1)
+distance = st.slider("Distance (in km):", min_value=0, max_value=50, value=10, step=1)
 
