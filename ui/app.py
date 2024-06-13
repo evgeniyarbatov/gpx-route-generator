105081e0c7f456324c649c345dd25addf450eba8
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-06-13 22:30:23 +0800

    Add lat and lon fields

--- ui/app.py
+++ ui/app.py
@@ -4,3 +4,19 @@ st.title('Route Generator')
 
 distance = st.slider("Distance (in km):", min_value=0, max_value=50, value=10, step=1)
 
+col1, col2 = st.columns(2)
+
+with col1:
+  start_lat = st.text_input(
+    "Start latitude:", 
+    value=1.3097970339490435, 
+  )
+
+with col2:
+  start_lng = st.text_input(
+    "Start longitude:", 
+    value=103.89455470068188, 
+  )
+
+
+
