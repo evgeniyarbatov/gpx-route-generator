9a1c29a98ff17d0c7223a6d3c34146f65652843d
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-12-18 11:33:42 +0700

    chore: format python code with black

--- ui/app.py
+++ ui/app.py
@@ -4,58 +4,52 @@ import streamlit as st
 
 from utils import create_routes, get_kml_destinations
 
-st.title('GPX Generator')
+st.title("GPX Generator")
 
-start = st.text_input(
-  "Start", 
-  value="1.3097970339490435, 103.89455470068188"
-)
+start = st.text_input("Start", value="1.3097970339490435, 103.89455470068188")
 
 input_choice = st.selectbox(
-  "Specify destination", 
-  [
-    "Location",
-    "KML file", 
-  ],
+    "Specify destination",
+    [
+        "Location",
+        "KML file",
+    ],
 )
 
 destinations = []
 if input_choice == "KML file":
-  kml_file = st.file_uploader(
-    "Locations", 
-    type=["kml"], 
-    accept_multiple_files=False,
-  )
-  if kml_file:
-    destinations = get_kml_destinations(kml_file)
+    kml_file = st.file_uploader(
+        "Locations",
+        type=["kml"],
+        accept_multiple_files=False,
+    )
+    if kml_file:
+        destinations = get_kml_destinations(kml_file)
 else:
-  stop = st.text_input(
-    "Stop", 
-    value="1.282100743659439, 103.85441068499448"
-  )
-  stop_lat, stop_lng = map(float, stop.split(","))
-  destinations.append(["Route", stop_lat, stop_lng])
+    stop = st.text_input("Stop", value="1.282100743659439, 103.85441068499448")
+    stop_lat, stop_lng = map(float, stop.split(","))
+    destinations.append(["Route", stop_lat, stop_lng])
 
 create = st.button(
-  "Create",
-  type="primary",
-  use_container_width=True,
+    "Create",
+    type="primary",
+    use_container_width=True,
 )
+
 
 def create_download_link(gpx, name):
     b64 = base64.b64encode(gpx.encode()).decode()
     href = f'<a href="data:application/octet-stream;base64,{b64}" download="{name}.gpx"><button>{name}.gpx</button></a>'
     return href
 
+
 if create:
     start_lat, start_lng = map(float, start.split(","))
     routes = create_routes(
-      start_lat, 
-      start_lng,
-      destinations,
+        start_lat,
+        start_lng,
+        destinations,
     )
     for name, fig, gpx in routes:
-      st.pyplot(fig)
-      st.markdown(create_download_link(gpx, name), unsafe_allow_html=True)
-
-
+        st.pyplot(fig)
+        st.markdown(create_download_link(gpx, name), unsafe_allow_html=True)
--- ui/utils.py
+++ ui/utils.py
@@ -10,57 +10,68 @@ import contextily as ctx
 import xml.dom.minidom
 from xml.etree.ElementTree import Element, SubElement, tostring
 
+
 def get_host():
-    is_in_docker = os.path.exists('/.dockerenv')
+    is_in_docker = os.path.exists("/.dockerenv")
     return "osrm:5000" if is_in_docker else "localhost:6000"
 
+
 def get_points(
-    start_lat, 
-    start_lng, 
+    start_lat,
+    start_lng,
     stop_lat,
     stop_lng,
 ):
     points = [(start_lat, start_lng)] + [(stop_lat, stop_lng)]
-    return points 
+    return points
+
 
 def osrm_format(coords):
     lat, lon = coords
     return f"{lon},{lat}"
-  
+
+
 def get_route(points):
     host = get_host()
-    points = ';'.join(map(osrm_format, points))
+    points = ";".join(map(osrm_format, points))
     params = {
-        'geometries': 'polyline6',
+        "geometries": "polyline6",
     }
-    
+
     response = requests.get(f"http://{host}/route/v1/foot/{points}", params=params)
     routes = response.json()
-    
-    if routes['code'] != 'Ok':
+
+    if routes["code"] != "Ok":
         return None
-    
-    geometry = routes['routes'][0]['geometry']
+
+    geometry = routes["routes"][0]["geometry"]
     return polyline.decode(geometry, 6)
- 
+
+
 def plot_gpx(route, ax):
     latitudes = [point[0] for point in route]
     longitudes = [point[1] for point in route]
-    ax.plot(longitudes, latitudes, color='red', label='Route')
-    ctx.add_basemap(ax, crs='EPSG:4326', source=ctx.providers.OpenStreetMap.Mapnik)
+    ax.plot(longitudes, latitudes, color="red", label="Route")
+    ctx.add_basemap(ax, crs="EPSG:4326", source=ctx.providers.OpenStreetMap.Mapnik)
     ax.legend()
     ax.set_yticks([])
     ax.set_xticks([])
-    ax.tick_params(axis='both', which='both', bottom=False, top=False, left=False, right=False)
+    ax.tick_params(
+        axis="both", which="both", bottom=False, top=False, left=False, right=False
+    )
+
 
 def create_gpx(route):
-    gpx = Element('gpx', {
-        'creator': 'StravaGPX',
-        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-        'xsi:schemaLocation': 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd',
-        'version': '1.1',
-        'xmlns': 'http://www.topografix.com/GPX/1/1'
-    })
+    gpx = Element(
+        "gpx",
+        {
+            "creator": "StravaGPX",
+            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "xsi:schemaLocation": "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd",
+            "version": "1.1",
+            "xmlns": "http://www.topografix.com/GPX/1/1",
+        },
+    )
     trk = SubElement(gpx, "trk")
     trkseg = SubElement(trk, "trkseg")
 
@@ -70,8 +81,9 @@ def create_gpx(route):
     gpx_file = xml.dom.minidom.parseString(
         tostring(gpx, encoding="unicode")
     ).toprettyxml()
- 
+
     return gpx_file
+
 
 def get_kml_destinations(file_path):
     destinations = []
@@ -99,35 +111,34 @@ def get_kml_destinations(file_path):
             longitude = float(coordinates[0])
 
             destinations.append([name.text, latitude, longitude])
-            
+
     return destinations
 
+
 def create_routes(
-    start_lat, 
+    start_lat,
     start_lng,
     destinations,
 ):
     routes = []
-    
+
     for destination in destinations:
         name, stop_lat, stop_lng = destination
-        
+
         points = get_points(
-            start_lat, 
-            start_lng, 
+            start_lat,
+            start_lng,
             stop_lat,
             stop_lng,
         )
 
-        osrm_route = get_route(points) 
-    
+        osrm_route = get_route(points)
+
         gpx = create_gpx(osrm_route)
-    
+
         fig, ax = plt.subplots(1, 1, figsize=(12, 30))
         plot_gpx(osrm_route, ax)
-        
-        routes.append(
-            (name, fig, gpx)
-        )
-    
+
+        routes.append((name, fig, gpx))
+
     return routes
